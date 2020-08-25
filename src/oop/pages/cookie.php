8ba1121993b7c0e85bfb67efe07bfa447ff90c78
Philip Hunkevych <60174953+ThenorBaker@users.noreply.github.com>
2020-08-25 11:59:23 +0300

    Class pages were added

---
+++ src/oop/pages/cookie.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once '../functions.php';
+require_once '../classes/Request.php';
+require_once '../classes/Cookie.php';
+
+$request = new Request($_GET, $_POST, $_SERVER);
+$cookie = new Cookie($_COOKIE);
+
+
+$cookie->placeholder = array_merge($cookie->placeholder, ['Hello' => 'World!',
+															'Hi' => 'there!',
+															'How' => 'are you?',
+															'Are you' => 'coding?']);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+    <?php require 'common/head.html' ?>
+
+    <body class="container main">
+
+        <?php require 'common/header.html' ?>
+
+        <main class="container ">
+
+            <div clas="ml-5 mt-1"> <!-- method's info -->
+            <?php
+            	$current_method = $request->query('method');
+                $current_arg = getArgCookie($current_method);
+                $method_signature = getSignature($current_method, require '../resources/cookie_methods_info.php');
+                $method_description = getDescription($current_method, require '../resources/cookie_methods_info.php');
+        	?>
+                <div>
+                    <article>
+                        <p>
+                            Method's signature: <span class="method_signature"> <?= $method_signature ?></span>
+                        </p>
+                        <hr>
+                        <p>
+                            What does it? <span class="method_description"> <?= $method_description ?></span>
+                        </p>
+                    </article>
+                </div>
+            </div>                 <!-- /method's info -->
+            <hr>
+
+            <div> <!-- method's input and output -->
+                <p>Result of calling  
+                    <span class="method_signature"> <?= $method_signature; ?></span> method with the 
+                    <span class="arg"> <?= var_dump($current_arg); ?> </span> key:
+                    <span class="method_calling ml-5"> <?php var_dump($cookie->$current_method($current_arg)); ?> </span>.
+                </p>
+            </div><!-- /method's input and output -->
+            <hr>
+
+        </main>
+
+        <?php require 'common/back_button.html'?>
+
+    </body>
+</html>
---
+++ src/oop/pages/session.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once '../functions.php';
+require_once '../classes/Request.php';
+require_once '../classes/Cookie.php';
+require_once '../classes/Session.php';
+
+if (!isset($_SESSION))
+  {
+    session_start();
+  }
+
+$request = new Request($_GET, $_POST, $_SERVER);
+$session = new Session($_SERVER);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+    <?php require 'common/head.html' ?>
+
+    <body class="container main">
+
+        <?php require 'common/header.html' ?>
+
+        <main class="container ">
+
+            <div clas="ml-5 mt-1"> <!-- method's info -->
+            <?php
+            	$current_method = $request->query('method');
+                $current_arg = getArgSession($current_method);
+                $method_signature = getSignature($current_method, require '../resources/session_methods_info.php');
+                $method_description = getDescription($current_method, require '../resources/session_methods_info.php');
+        	?>
+                <div>
+                    <article>
+                        <p>
+                            Method's signature: <span class="method_signature"> <?= $method_signature ?></span>
+                        </p>
+                        <hr>
+                        <p>
+                            What does it? <span class="method_description"> <?= $method_description ?></span>
+                        </p>
+                    </article>
+                </div>
+            </div>                 <!-- /method's info -->
+            <hr>
+
+            <div> <!-- method's input and output -->
+                <p>Result of calling  
+                    <span class="method_signature"> <?= $method_signature; ?></span> method with the 
+                    <span class="arg"> <?= var_dump($current_arg); ?> </span> key:
+                    <span class="method_calling ml-5"> <?php var_dump($session->$current_method($current_arg)); ?> </span>.
+                </p>
+            </div><!-- /method's input and output -->
+            <hr>
+
+        </main>
+
+        <?php require 'common/back_button.html'?>
+
+    </body>
+</html>
