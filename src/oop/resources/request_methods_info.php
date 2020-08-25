2e6d90fdc67631883e295b7b307551b69bf9b7f4
Philip Hunkevych <60174953+ThenorBaker@users.noreply.github.com>
2020-08-25 12:01:43 +0300

    Method's information was added to the separate files

---
+++ src/oop/resources/request_methods_info.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    [
+        'name' => 'query($key, $default = null)',
+        'description' => 'Returns $_GET parameter by $key and $default if does not exist.',
+    ],
+    [
+        'name' => 'post($key, $default = null)',
+        'description' => 'Returns $_POST parameter by $key and $default if does not exist.',
+    ],
+    [
+        'name' => 'get($key, $default = null)',
+        'description' => 'Returns $_GET or $_POST parameter by $key. If both are present - return $_POST. If both are empty - return $default.',
+    ],
+    [
+        'name' => 'all(array $only = [])',
+        'description' => 'Returns all $_GET + $_POST parameters in the associative array. If $only is not empty - return only keys from $only parameter.',
+    ],
+    [
+        'name' => 'has($key)',
+        'description' => 'Return true if $key exists in $_GET or $_POST.',
+    ],
+    [
+        'name' => 'ip()',
+        'description' => 'Returns users IP.',
+    ],
+    [
+        'name' => 'userAgent()',
+        'description' => 'Returns users browser User Agent.',
+    ],
+
+];
