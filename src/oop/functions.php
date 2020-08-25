2d5f6d741e715b46ce71f9b225af63bc522e9236
Philip Hunkevych <60174953+ThenorBaker@users.noreply.github.com>
2020-08-25 12:00:37 +0300

    'functions.php' was created

---
+++ src/oop/functions.php
@@ -0,0 +1,115 @@
+<?php
+
+function debug($data){
+    return '<pre>' . print_r($data, 1) . '</pre>';
+}
+
+function getDescription($current_method, $method_description)
+{
+    foreach ($method_description as $key => $value){
+        $cutBeforeParentheses = stristr($value['name'], '(', true);
+        if($cutBeforeParentheses == $current_method){
+            return $value['description'];
+        }
+    }
+}
+
+function getSignature($current_method, $method_description)
+{
+    foreach ($method_description as $key => $value){
+        $cutBeforeParentheses = stristr($value['name'], '(', true);
+        if($cutBeforeParentheses == $current_method){
+            return $value['name'];
+        }
+    }
+}
+
+function isExists($filteringKey, $merged)
+{
+    foreach ($merged as $key => $value) {
+        if($key == $filteringKey){
+            return true;
+        }
+    }
+}
+
+function getArgRequest($method)
+{
+    $arg = null;
+
+    switch ($method) {
+        case 'get':
+            $arg = 'method';
+            break;
+        case 'all':
+            $arg = ['method'];
+            break;
+        case 'has':
+            $arg = 'method';
+            break;
+        case 'query':
+            $arg = 'method';
+            break;
+        case 'post':
+            $arg = 'some_arg';
+            break;
+        default:
+            $arg = null;
+        break;
+    }
+    return $arg;
+}
+
+function getArgCookie($method)
+{
+    $arg = null;
+
+    switch ($method) {
+        case 'get':
+            $arg = 'Hello';
+            break;
+        case 'all':
+            $arg = ['Are you'];
+            break;
+        case 'has':
+            $arg = 'PHPSESSID';
+            break;
+        case 'set':
+            $arg = 'cookie_setter_test';
+            break;
+        case 'remove':
+            $arg = 'cookie_setter_test';
+            break;
+        default:
+            $arg = null;
+        break;
+    }
+    return $arg;
+}
+
+function getArgSession($method)
+{
+    $arg = null;
+
+    switch ($method) {
+        case 'get':
+            $arg = 'QUERY_STRING';
+            break;
+        case 'all':
+            $arg = ['DOCUMENT_ROOT'];
+            break;
+        case 'has':
+            $arg = 'HTTP_USER_AGENT';
+            break;
+        case 'set':
+            $arg = 'NEW_PLACE';
+            break;
+        case 'remove':
+            $arg = 'SERVER_PROTOCOL';
+            break;
+        default:
+            $arg = null;
+        break;
+    }
+    return $arg;
+}
